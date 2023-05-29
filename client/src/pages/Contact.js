f8bdc5cc38c62abfc04bdcb5c36f10151d4cf3c7
King-OJ <clementojigs@hotmail.com>
2023-05-29 16:31:01 +0200

    adjusted contact form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -32,13 +32,14 @@ export default function Contact() {
               <textarea name='message' id="message" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-[#9E679E] appearance-none dark:text-white dark:focus:border-[#9E679E] focus:outline-none focus:ring-0 focus:border-[#9E679E] peer" placeholder=" " />
               <label htmlFor="message" className="absolute text-sm text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Message</label>
             </div>
-            <button type='submit' disabled={state.submitting} className="cta-btn w-full">Send Proposal</button>
-          </form>
-          {success && 
+            {success && 
             <div className="text-center my-10 text-green-500 ">
             Thanks for contacting me! I'll give a response as soon as I get this!
             </div>  
           } 
+            <button type='submit' disabled={state.submitting} className="cta-btn w-full">Send Proposal</button>
+          </form>
+          
         </div>    
     </main>
     );
@@ -61,13 +62,15 @@ export default function Contact() {
               <textarea name='message' id="message" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-[#9E679E] appearance-none dark:text-white dark:focus:border-[#9E679E] focus:outline-none focus:ring-0 focus:border-[#9E679E] peer" placeholder=" " />
               <label htmlFor="message" className="absolute text-sm text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Message</label>
             </div>
-            <button type='submit' disabled={state.submitting} className="cta-btn w-full">Send Proposal</button>
-          </form> 
-          {success &&
+            
+            {success &&
             <div className="text-center my-10 text-red-500 ">
             There was an error! Try Again!!!
           </div>   
           }
+            <button type='submit' disabled={state.submitting} className="cta-btn w-full">Send Proposal</button>
+          </form> 
+          
         </div>    
     </main>
     );
