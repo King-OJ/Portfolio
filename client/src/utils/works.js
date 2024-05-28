fc0ebfe6774dafc215b1a78c6bdc2bbcf2890d81
Clement Ojiguo <clementojigs@hotmail.com>
2024-05-28 15:26:16 -0700

    added new project

--- client/src/utils/works.js
+++ client/src/utils/works.js
@@ -1,69 +1,77 @@
-import comfyImg from '../assets/images/comfy.png'
-import jobsterImg from '../assets/images/jobster.png'
-import loopstudiosImg from '../assets/images/loopstudios.png'
-import fyloImg from '../assets/images/fylo.png'
-import githubImg from '../assets/images/github.png'
-import bookmarkImg from '../assets/images/bookmark.png'
-import gpt3Img from '../assets/images/gpt3.png'
-import jobifyImg from '../assets/images/jobify1.png'
+import comfyImg from "../assets/images/comfy.png";
+import jobsterImg from "../assets/images/jobster.png";
+import loopstudiosImg from "../assets/images/loopstudios.png";
+import fyloImg from "../assets/images/fylo.png";
+import githubImg from "../assets/images/github.png";
+import bookmarkImg from "../assets/images/bookmark.png";
+import gpt3Img from "../assets/images/gpt3.png";
+import jobifyImg from "../assets/images/jobify1.png";
+import thrivelyImg from "../assets/images/thrivelyImg.png";
 
 const works = [
-    {
-      img: comfyImg,
-      title: 'E-Commerce Website',
-      alt: 'comfy sloth',
-      liveLink: 'https://comfy-ecommerce-ng.netlify.app/',
-      codeLink: 'https://github.com/King-OJ/e-commerce-comfy-furnitures'
-    },
-    {
-      img: jobifyImg,
-      title: 'Jobs Tracking Website',
-      alt: 'jobster',
-      liveLink: 'https://clem-fullstack-jobify.onrender.com/',
-      codeLink: 'https://github.com/King-OJ/my_jobify'
-    },
-    {
-      img: jobsterImg,
-      title: 'Jobs Application Portal',
-      alt: 'jobify',
-      liveLink: 'https://my-jobster-app.vercel.app/',
-      codeLink: 'https://github.com/King-OJ/my_jobster'
-    },
-    {
-      img: githubImg,
-      title: 'Github Simulated App',
-      alt: 'github clone',
-      liveLink: 'https://clems-github-search-users-app.netlify.app/',
-      codeLink: 'https://github.com/King-OJ/search-github-users-react-app'
-    },
-    {
-      img: loopstudiosImg,
-      title: 'Loop Studios Clone',
-      alt: 'loo studios clone',
-      liveLink: 'https://loopstudios-five-rho.vercel.app/',
-      codeLink: 'https://github.com/King-OJ/loopstudios'
-    },
-    {
-      img: gpt3Img,
-      title: 'Chat GPT3 Clone',
-      alt: 'Chat GPT3 Clone',
-      liveLink: 'https://gpt3-react-modern-ui.netlify.app/',
-      codeLink: 'https://github.com/King-OJ/gpt3ModernUI'
-    },
-    {
-      img: fyloImg,
-      title: 'Fylo Clone',
-      alt: 'Fylo Clone',
-      liveLink: 'https://fylo-nine-sage.vercel.app/',
-      codeLink: 'https://github.com/King-OJ/fylo'
-    },
-    {
-      img: bookmarkImg,
-      title: 'Bookmark Clone',
-      alt: 'Bookmark Clone',
-      liveLink: 'https://bookmark-react-tailwind-website.netlify.app/',
-      codeLink: 'https://github.com/King-OJ/bookmark-tailwind-css'
-    },
-  ]
+  {
+    img: comfyImg,
+    title: "Thrively Landing",
+    alt: "thrively page",
+    liveLink: "https://thrively-six.vercel.app/",
+    codeLink: "https://github.com/King-OJ/thrively",
+  },
+  {
+    img: comfyImg,
+    title: "E-Commerce Website",
+    alt: "comfy sloth",
+    liveLink: "https://comfy-ecommerce-ng.netlify.app/",
+    codeLink: "https://github.com/King-OJ/e-commerce-comfy-furnitures",
+  },
+  {
+    img: jobifyImg,
+    title: "Jobs Tracking Website",
+    alt: "jobster",
+    liveLink: "https://clem-fullstack-jobify.onrender.com/",
+    codeLink: "https://github.com/King-OJ/my_jobify",
+  },
+  {
+    img: jobsterImg,
+    title: "Jobs Application Portal",
+    alt: "jobify",
+    liveLink: "https://my-jobster-app.vercel.app/",
+    codeLink: "https://github.com/King-OJ/my_jobster",
+  },
+  {
+    img: githubImg,
+    title: "Github Simulated App",
+    alt: "github clone",
+    liveLink: "https://clems-github-search-users-app.netlify.app/",
+    codeLink: "https://github.com/King-OJ/search-github-users-react-app",
+  },
+  {
+    img: loopstudiosImg,
+    title: "Loop Studios Clone",
+    alt: "loo studios clone",
+    liveLink: "https://loopstudios-five-rho.vercel.app/",
+    codeLink: "https://github.com/King-OJ/loopstudios",
+  },
+  {
+    img: gpt3Img,
+    title: "Chat GPT3 Clone",
+    alt: "Chat GPT3 Clone",
+    liveLink: "https://gpt3-react-modern-ui.netlify.app/",
+    codeLink: "https://github.com/King-OJ/gpt3ModernUI",
+  },
+  {
+    img: fyloImg,
+    title: "Fylo Clone",
+    alt: "Fylo Clone",
+    liveLink: "https://fylo-nine-sage.vercel.app/",
+    codeLink: "https://github.com/King-OJ/fylo",
+  },
+  {
+    img: bookmarkImg,
+    title: "Bookmark Clone",
+    alt: "Bookmark Clone",
+    liveLink: "https://bookmark-react-tailwind-website.netlify.app/",
+    codeLink: "https://github.com/King-OJ/bookmark-tailwind-css",
+  },
+];
 
-  export default works
+export default works;
